43f8342800501fb143c2cc24e21eab97c375fa34
João Vitor Albero <contato.joaoalbero@gmail.com>
2025-11-17 15:05:20 -0300

    Adding data.js content / Adicionando o conteúdo do data.js

---
+++ src/fakebank/js/data.js
@@ -0,0 +1,34 @@
+// src/fakebank/js/data.js
+
+const FAKE_USER = {
+  name: "João Albero",
+  balance: 3250.75,
+  account: "0001-1",
+};
+
+const FAKE_TRANSACTIONS = [
+  {
+    date: "12/11/2025",
+    description: "PIX • Bar do Russo",
+    type: "debit",
+    amount: 89.9,
+  },
+  {
+    date: "11/11/2025",
+    description: "Crédito salário • Livestrong",
+    type: "credit",
+    amount: 3500.0,
+  },
+  {
+    date: "10/11/2025",
+    description: "Netflix",
+    type: "debit",
+    amount: 39.9,
+  },
+  {
+    date: "08/11/2025",
+    description: "Compra online • Kabum",
+    type: "debit",
+    amount: 579.99,
+  },
+];
