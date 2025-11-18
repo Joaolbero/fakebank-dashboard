17256d0d9e4bf3169394025e8f3652bde428bfd7
João Vitor Albero <contato.joaoalbero@gmail.com>
2025-11-18 14:37:17 -0300

    Updating the data.js / Atualizando o data.js

--- src/fakebank/js/data.js
+++ src/fakebank/js/data.js
@@ -1,7 +1,7 @@
 const FAKE_USER = {
   name: "João Albero",
   balance: 3250.75,
-  account: "0001-1",
+  account: "0001-1"
 };
 
 const FAKE_TRANSACTIONS = [
@@ -9,26 +9,26 @@ const FAKE_TRANSACTIONS = [
     date: "12/11/2025",
     description: "PIX • Bar do Russo",
     type: "debit",
-    amount: 89.9,
+    amount: 89.9
   },
   {
     date: "11/11/2025",
     description: "Crédito salário • Livestrong",
     type: "credit",
-    amount: 3500.0,
+    amount: 3500.0
   },
   {
     date: "10/11/2025",
     description: "Netflix",
     type: "debit",
-    amount: 39.9,
+    amount: 39.9
   },
   {
     date: "08/11/2025",
     description: "Compra online • Kabum",
     type: "debit",
-    amount: 579.99,
-  },
+    amount: 579.99
+  }
 ];
 
 const FAKE_SPENDING_CATEGORIES = [
@@ -37,3 +37,6 @@ const FAKE_SPENDING_CATEGORIES = [
   { label: "Lazer", amount: 250 },
   { label: "Online", amount: 160 }
 ];
+
+let CURRENT_USER_BALANCE = FAKE_USER.balance;
+let CURRENT_TRANSACTIONS = FAKE_TRANSACTIONS.slice();
